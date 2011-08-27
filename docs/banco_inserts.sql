3a93af5781e07e1bbe09cbda6623b79554d7c9d1
Luiz Sanches <luizgrsanches@gmail.com>
2011-08-27 16:39:30 -0300

    remoção de campos das tabelas do banco

--- docs/banco_inserts.sql
+++ docs/banco_inserts.sql
@@ -1,3 +1,2 @@
-insert into tipo_inscricao (descricao, valor, status) values ('Estudante', 0.5, 'A');
-insert into tipo_inscricao (descricao, valor, status) values ('Profissional', 1, 'A');
+insert into tipo_inscricao (descricao, valor, status) values ('Valor unico', 0.5, 'A');
 insert into tipo_inscricao (descricao, valor, status) values ('Cortesia', 0, 'I');
--- docs/banco_tabelas.sql
+++ docs/banco_tabelas.sql
@@ -7,7 +7,7 @@ CREATE  TABLE IF NOT EXISTS tipo_inscricao (
   status CHAR(1) NOT NULL DEFAULT 'A',
   PRIMARY KEY (id) ,
   UNIQUE INDEX descricao_UNIQUE (descricao ASC) )
-ENGINE = InnoDB
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
 AUTO_INCREMENT = 1;
 
 
@@ -29,24 +29,7 @@ CREATE  TABLE IF NOT EXISTS inscricao (
     REFERENCES tipo_inscricao (id )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-PACK_KEYS = DEFAULT;
-
-
-DROP TABLE IF EXISTS endereco ;
-
-CREATE  TABLE IF NOT EXISTS endereco (
-  id INT NOT NULL AUTO_INCREMENT ,
-  endereco VARCHAR(60) NOT NULL ,
-  numero INT NOT NULL ,
-  complemento VARCHAR(60) NULL ,
-  bairro VARCHAR(45) NOT NULL ,
-  cep VARCHAR(8) NOT NULL ,
-  cidade VARCHAR(45) NOT NULL ,
-  uf VARCHAR(2) NOT NULL ,
-  PRIMARY KEY (id) )
-ENGINE = InnoDB
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
 AUTO_INCREMENT = 1;
 
 
@@ -54,26 +37,15 @@ DROP TABLE IF EXISTS empresa ;
 
 CREATE  TABLE IF NOT EXISTS empresa (
   id INT NOT NULL AUTO_INCREMENT ,
-  cnpj VARCHAR(14) NOT NULL ,
-  razao_social VARCHAR(100) NULL ,
-  nome_fantasia VARCHAR(100) NOT NULL ,
-  nome_responsavel VARCHAR(60) NOT NULL ,
-  telefone VARCHAR(10) NOT NULL ,
+  nome VARCHAR(100) NOT NULL ,
+  responsavel VARCHAR(50) NOT NULL ,
   email VARCHAR(45) NOT NULL ,
-  senha VARCHAR(45) NOT NULL ,
-  id_endereco INT NOT NULL ,
-  ddd INT NOT NULL ,
+  cep CHAR(8) NOT NULL , 
   PRIMARY KEY (id) ,
-  UNIQUE INDEX razao_social_UNIQUE (razao_social ASC) ,
+  UNIQUE INDEX nome_UNIQUE (nome ASC) ,
   UNIQUE INDEX cnpj_UNIQUE (cnpj ASC) ,
-  UNIQUE INDEX email_UNIQUE (email ASC) ,
-  INDEX fk_empresa_endereco1 (id_endereco ASC) ,
-  CONSTRAINT fk_empresa_endereco1
-    FOREIGN KEY (id_endereco )
-    REFERENCES endereco (id )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
+  UNIQUE INDEX email_UNIQUE (email ASC) )
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
 AUTO_INCREMENT = 1;
 
 
@@ -82,31 +54,20 @@ DROP TABLE IF EXISTS individual ;
 CREATE  TABLE IF NOT EXISTS individual (
   id INT NOT NULL AUTO_INCREMENT ,
   nome VARCHAR(60) NOT NULL ,
-  cpf VARCHAR(11) NOT NULL ,
-  email VARCHAR(45) NOT NULL ,
-  nome_cracha VARCHAR(60) NOT NULL ,
-  senha VARCHAR(20) NOT NULL ,
-  empresa VARCHAR(100) NOT NULL ,
-  ddd INT NOT NULL ,
-  telefone VARCHAR(10) NOT NULL ,
+  email VARCHAR(50) NOT NULL ,
+  instituicao VARCHAR(100) NOT NULL ,
+  cep CHAR(8) NOT NULL ,
   sexo CHAR(1) NOT NULL DEFAULT 'M',
   situacao CHAR(1) NOT NULL DEFAULT 'A',
   id_inscricao INT NOT NULL ,
-  id_endereco INT NOT NULL ,
   UNIQUE INDEX cpf_UNIQUE (cpf ASC) ,
   UNIQUE INDEX email_UNIQUE (email ASC) ,
   PRIMARY KEY (id) ,
   INDEX fk_individual_inscricao1 (id_inscricao ASC) ,
-  INDEX fk_individual_endereco1 (id_endereco ASC) ,
   CONSTRAINT fk_individual_inscricao1
     FOREIGN KEY (id_inscricao )
     REFERENCES inscricao (id )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_individual_endereco1
-    FOREIGN KEY (id_endereco )
-    REFERENCES endereco (id )
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
 AUTO_INCREMENT = 1;
